120a78bbccf4f1db20959d2bf123c0bcb4a035cb
Feras Aldahlawi <feras@chromium.org>
2024-10-19 23:25:43 -0700

    Remove unused vsnip configuration

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -4,11 +4,6 @@ cmp.setup({
       native_menu = false,
       ghost_text = true,
   },
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
   mapping = {
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
