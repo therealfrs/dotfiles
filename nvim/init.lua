caf9ecca9196d64cf450c84b86afc2c610b725a3
Feras Aldahlawi <frs@chromium.org>
2025-08-11 12:11:09 -0700

    Fix relative autoninja build error to quickfix

--- nvim/init.lua
+++ nvim/init.lua
@@ -41,15 +41,30 @@ function set_ninja_make()
   local cwd = vim.fn.getcwd()
   if vim.fn.filereadable(vim.fn.glob(cwd .. "/BUILD.gn"))
   then
-    vim.o.makeprg = 'autoninja -C '
-    -- vim.api.nvim_buf_set_option(0, 'makeprg', 'autoninja -C ')
-    local efm = vim.api.nvim_buf_get_option(0, 'errorformat')
-    efm = "%*[./]%f:%l:%c: error: %m,%-G%.%#" .. efm
-    vim.o.errorformat = efm
-    -- vim.api.nvim_buf_set_option(0, 'errorformat', efm)
+    vim.o.makeprg = 'autoninja -C'
   end
 end
 set_ninja_make()
+
+vim.api.nvim_create_augroup("NinjaMake", { clear = true })
+vim.api.nvim_create_autocmd("QuickFixCmdPre", {
+  pattern = "make",
+  group = "NinjaMake",
+  callback = function()
+    -- old_efm_for_ninja = nil
+    -- Check if we are using the ninja makeprg
+    if string.find(vim.o.makeprg, "autoninja -C") then
+      -- Get the arguments passed to :make (e.g., "out/Default")
+      local args = vim.fn.expand("<afile>")
+      -- The first argument to make is the build directory.
+      local build_dir = vim.fn.split(args, " ")[1]
+      -- Check that the directory actually exists
+      if build_dir and build_dir ~= "" and vim.fn.isdirectory(build_dir) == 1 then
+        vim.opt.errorformat:prepend("%D" .. vim.fn.getcwd() .. "/" .. build_dir)
+      end
+    end
+  end,
+})
 
 
 -- disable lsp highlight
